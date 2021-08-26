7eb59d6d7664f838429721376ded5056aca0f252
Beckem <boychienga12@gmail.com>
2021-08-27 01:35:48 +0700

    Implement drag and drop, eslint

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,15 +1,39 @@
 import Card from 'components/Card/Card'
 import React from 'react'
 import './Column.scss'
+import { Container, Draggable } from 'react-smooth-dnd'
 import {mapOrder} from 'utilites/sorts'
 function Column({column}) {
     const cards=mapOrder(column.cards, column.cardOrder, 'id');
+
+    const onCardDrop = (dropResult) => {
+        console.log(dropResult);
+    }
+
     return (
         <div className="column">
-            <header>{column.title}</header>
-            <ul className="card-list">
-                {cards.map((card, index) => <Card key={index} card={card}/>)}
-            </ul>
+            <header className="column-drag-handle">{column.title}</header>
+            <div className="card-list">
+                <Container
+                    groupName="col"
+                    onDrop={onCardDrop}
+                    getChildPayload={index => cards[index]}
+                    dragClass="card-ghost"
+                    dropClass="card-ghost-drop"
+                    dropPlaceholder={{
+                        animationDuration: 150,
+                        showOnTop: true,
+                        className: 'card-drop-preview'
+                    }}
+                    dropPlaceholderAnimationDuration={200}
+                >
+                    {cards.map((card, index) => (
+                        <Draggable key={index}>
+                            <Card card={card}/>
+                        </Draggable>
+                    ))}
+                </Container>
+            </div>
             <footer>Add another card</footer>
             </div>
     )
